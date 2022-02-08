b89fb5390c863ad8f3c9cb8c4eb6d860795ae58a
YurginMichael <rezov18@gmail.com>
2022-02-08 17:34:53 +0300

    'myCommit'

---
+++ src/Pages/About.js
@@ -0,0 +1,9 @@
+import React,{Fragment} from "react";
+
+export const About=()=>{
+    return(
+        <Fragment>
+            <h1>Home page</h1>
+        </Fragment>
+    )
+}
---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React,{Fragment} from "react";
+
+
+export const Home=()=>{
+
+    return(
+        <Fragment>
+            <h1>dpspdpddp</h1>
+        </Fragment>
+    )
+}
